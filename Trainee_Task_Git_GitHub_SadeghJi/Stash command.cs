7c53e61868f7cf68a4b459c5c0351a16e5c57473
Sadegh Jafari <sadeghji1998@gmail.com>
2024-11-12 01:13:28 +0330

    Temporary archive of changes in Stash

---
+++ Trainee_Task_Git_GitHub_SadeghJi/Stash command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainee_Task_Git_GitHub_SadeghJi
+{
+    internal class Stash_command
+    {
+        
+
+    }
+}
