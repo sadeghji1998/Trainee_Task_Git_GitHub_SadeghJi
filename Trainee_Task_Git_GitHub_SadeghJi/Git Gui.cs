79777df160684597e9769c08e6423503f561b9ed
Sadegh Jafari <sadeghji1998@gmail.com>
2024-11-12 01:10:50 +0330

    Record the first commit with Git Gui and GitK

---
+++ Trainee_Task_Git_GitHub_SadeghJi/Git Gui.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainee_Task_Git_GitHub_SadeghJi
+{
+    internal class Git_Gui
+    {
+        public int  GitK { get; set; }
+    }
+}
