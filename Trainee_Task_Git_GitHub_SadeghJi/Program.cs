e8d1a87035aa36b10d75653d7da612d187a33239
Sadegh Jafari <sadeghji1998@gmail.com>
2024-11-11 23:37:46 +0330

    First commit recorded

--- Trainee_Task_Git_GitHub_SadeghJi/Program.cs
+++ Trainee_Task_Git_GitHub_SadeghJi/Program.cs
@@ -10,6 +10,16 @@ namespace Trainee_Task_Git_GitHub_SadeghJi
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Product A", Category = Categories.Category1, Price = 100 },
+                new Product { Id = 2, Name = "Product B", Category = Categories.Category1, Price = 150 },
+                new Product { Id = 3, Name = "Product C", Category = Categories.Category2, Price = 120 },
+                new Product { Id = 4, Name = "Product D", Category = Categories.Category3, Price = 200 },
+                new Product { Id = 5, Name = "Product E", Category = Categories.Category1, Price = 80 }
+            };
+
+
         }
     }
 }
