087f7851f7bf0243cd62cce223ef53b2c72797a1
Sadegh Jafari <sadeghji1998@gmail.com>
2024-11-12 00:38:20 +0330

    Registration of tags in Git and in this commit

---
+++ Trainee_Task_Git_GitHub_SadeghJi/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainee_Task_Git_GitHub_SadeghJi
+{
+    internal class Tag
+    {
+        public int MyProperty { get; set; }
+    }
+}
