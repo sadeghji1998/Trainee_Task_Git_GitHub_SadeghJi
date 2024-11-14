2d82f2284aef10da5d80794ac3cbf1183b557acd
Sadegh Jafari <sadeghji1998@gmail.com>
2024-11-15 01:21:30 +0330

    Check and fix Conflict in Cherry Pick

---
+++ Trainee_Task_Git_GitHub_SadeghJi/Cherry Pick.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trainee_Task_Git_GitHub_SadeghJi
+{
+    internal class Cherry_Pick
+    {
+        public int Conflict { get; set; }
+    }
+}
